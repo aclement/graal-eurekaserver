f07b9b768b37a86ba2f47324d4087ebc61315c61
Andy Clement <aclement@pivotal.io>
2020-07-03 09:29:35 -0700

    tweak for showing problem on java11

--- jo/ni.sh
+++ jo/ni.sh
@@ -1,1 +1,1 @@
-native-image -H:+ReportExceptionStackTraces --no-fallback -cp jsr311-api-1.1.1.jar:jersey-core-1.19.1.jar:jerset-client-1.19.1.jar:target/classes Run
+native-image -H:+ReportExceptionStackTraces --no-fallback -cp jakarta.activation-api-1.2.2.jar:jsr311-api-1.1.1.jar:jersey-core-1.19.1.jar:jersey-client-1.19.1.jar:target/classes Run
